99d0608cabd288afb0f82cd1cad1a98f8d9940a9
Marcus van Houdt <mjvh80@gmail.com>
2016-03-04 09:55:03 +0100

    Update to FSharp framework 4. - Update nuget - Use custom nuget package version (package not intended for public nuget release)

--- FsYaml/AssemblyInfo.fs
+++ FsYaml/AssemblyInfo.fs
@@ -8,9 +8,11 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyDescription("Type safe Yaml library for F#.")>]
 [<assembly: Guid("6852C155-6DAA-4CC2-B44A-BD83B23D17C0")>]
 
-[<assembly: AssemblyVersion("2.0.1.0")>]
-[<assembly: AssemblyFileVersion("2.0.1.0")>]
-[<assembly: AssemblyInformationalVersion("2.0.1.0")>]
+[<assembly: AssemblyVersion("2.0.2.0")>]
+[<assembly: AssemblyFileVersion("2.0.2.0")>]
+
+// NOTE: nuget pack uses this as its nuget version
+[<assembly: AssemblyInformationalVersion("2.0.2-mjcustom")>]
 
 [<assembly: InternalsVisibleTo("FsYaml.Tests")>]
 ()
